6c7ba7bfd63bda4ecf24291e655ee33560941d26
Olusoladeboy <eoyewole508@stu.ui.edu.ng>
2020-04-11 02:16:43 +0100

    Changes done

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,12 @@ const socket = io()
 
 const roomTemplate = document.querySelector('#rooms-template').innerHTML
 const roomSelect = document.querySelector('#room-select')
+const $roomSelect = document.querySelector('#roomSelect')
+
+$roomSelect.addEventListener('click', () => {
+    const selected = $roomSelect[$roomSelect.selectedIndex].value;
+    console.log(selected)
+})
 
 socket.on('rooms', (rooms) => {
     console.log(rooms)
